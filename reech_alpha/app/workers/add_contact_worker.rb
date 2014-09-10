4c0dc58898b33b58937fbf402cb3afd915f6696b
srinivas <kpsrinivas@gmail.com>
2014-09-10 11:10:13 +0530

    Exception handling for add contact worker.

--- reech_alpha/app/workers/add_contact_worker.rb
+++ reech_alpha/app/workers/add_contact_worker.rb
@@ -3,19 +3,19 @@ class AddContactWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
   def perform(email, phone, user_id)
-    user = User.find(user_id)
-    UserMailer.send_invitation_email_for_new_contact(email, user).deliver if email
-    if phone
-      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
-      begin
-        sms = client.account.sms.messages.create(
-            from: TWILIO_CONFIG['from'],
-            to: phone,
-            body: "your friend #{user.first_name} #{user.last_name} needs to add you as a contact on Reech."
-        )
-      rescue Exception => e
-        puts e.backtrace.join("\n")
+    begin
+      user = User.find(user_id)
+      UserMailer.send_invitation_email_for_new_contact(email, user).deliver if email
+      if phone
+        client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+          sms = client.account.sms.messages.create(
+              from: TWILIO_CONFIG['from'],
+              to: phone,
+              body: "your friend #{user.first_name} #{user.last_name} needs to add you as a contact on Reech."
+          )
       end
+    rescue Exception => e
+      puts e.backtrace.join("\n")
     end
   end
 end
