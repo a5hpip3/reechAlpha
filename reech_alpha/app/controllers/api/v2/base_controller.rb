cee077e1664b3fa8c361af088af01c659d574b3f
srinivas <kpsrinivas@gmail.com>
2014-09-19 17:46:48 +0530

    Disable the complete API key logic for authentication. Use the secret session generated by Rails.

--- reech_alpha/app/controllers/api/v2/base_controller.rb
+++ reech_alpha/app/controllers/api/v2/base_controller.rb
@@ -4,19 +4,18 @@ module Api
 
       private
       def require_current_user
-				render :status => 401, :json => {error: "Not authenticated"} unless check_auth
+				render status: 401, json: {error: "Not authenticated"} unless current_user
 			end
 
-			def check_auth
-				unless @current_user 
-				  key = ApiKey.find_by_access_token_and_user_id(params[:api_key], params[:user_id])
-				  @current_user = key ? User.find_by_reecher_id(params[:user_id]) : nil 
-				end
-			end
+			def current_user_session
+        return @current_user_session if defined?(@current_user_session)
+        @current_user_session = UserSession.find
+      end
 
-			def current_user
-				@current_user
-			end
+      def current_user
+        return @current_user if defined?(@current_user)
+        @current_user = current_user_session && current_user_session.user
+      end
     end
   end
 end
--- reech_alpha/app/controllers/api/v2/sessions_controller.rb
+++ reech_alpha/app/controllers/api/v2/sessions_controller.rb
@@ -5,13 +5,10 @@ module Api
 			def create
 				user_session = UserSession.new(params[:session][:user_details])
 				if user_session.save
-					user = User.where(phone_number: user_session.phone_number).first
-					api_key = ::ApiKey.find_or_create_by_user_id(user.reecher_id).access_token
-					render json: {user: user, user_profile: user.user_profile, api_key: api_key}
+					render json: {user: current_user, user_profile: current_user.user_profile}
 				else
-					render json: user_session.errors
+					render status: 401, json: {errors: user_session.errors}
 				end
-
 			end
 
 		end
