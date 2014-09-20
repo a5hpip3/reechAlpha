2d274a3e9c5f0181e840cdf7f4d85fc649b8918c
srinivas <kpsrinivas@gmail.com>
2014-09-20 18:02:55 +0530

    Changes for authentication.

--- reech_alpha/app/controllers/api/v2/sessions_controller.rb
+++ reech_alpha/app/controllers/api/v2/sessions_controller.rb
@@ -3,6 +3,16 @@ module Api
 		class SessionsController < ::Devise::SessionsController
 			respond_to :json
 
+			def create
+				self.resource = warden.authenticate!({ :scope => resource_name, :recall => "#{controller_path}#failure" })
+		    sign_in(resource_name, resource)
+		    render json: {success: true, user: resource}
+			end
+
+			def failure
+				render status: 401, json: {success: false, errors: "Invalid username/ password."}
+			end
+
 		end
 	end
 end
