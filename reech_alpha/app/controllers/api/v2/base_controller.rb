dda4cd467040f04201899d47efd12107c05c0b0a
srinivas <kpsrinivas@gmail.com>
2014-09-22 09:30:24 +0530

    Disable devise fallback if authentication fails.

--- reech_alpha/app/controllers/api/v2/base_controller.rb
+++ reech_alpha/app/controllers/api/v2/base_controller.rb
@@ -1,11 +1,11 @@
 module Api
   module V2
     class BaseController < CrudController
-      acts_as_token_authentication_handler_for User
+      acts_as_token_authentication_handler_for User, fallback_to_devise: false
 
       private
       def require_current_user
-        
+
 				render status: 401, json: {error: "Not authenticated"} unless current_user
 			end
 
