d552a3be8e5c36d26b3c2b13f5bba6a90dc1639d
srinivas <kpsrinivas@gmail.com>
2014-09-30 09:18:48 +0530

    conflict.

--- reech_alpha/db/migrate/20140826061641_add_is_public_to_solution_tables.rb
+++
@@ -1,7 +0,0 @@
-class AddIsPublicToSolutionTables < ActiveRecord::Migration
-  def change
-    #solution is public by default if :is_public =1
-    #solution is private if :is_public =0
-    #add_column  :solutions, :is_public, :boolean ,:default =>1,:after=>:ask_charisma
-  end
-end
