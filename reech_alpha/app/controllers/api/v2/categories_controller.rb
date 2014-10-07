d86eb0ef11b662fe3e0101906568c8db1f71aec5
srinivas <kpsrinivas@gmail.com>
2014-10-07 09:42:22 +0530

    Check for current user in categories.

--- reech_alpha/app/controllers/api/v2/categories_controller.rb
+++ reech_alpha/app/controllers/api/v2/categories_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V2
     class CategoriesController < BaseController
+      before_filter :require_current_user
+      
       def index
         render "index.json.jbuilder"
       end
