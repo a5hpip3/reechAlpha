38b2d702135b2b151cf0dd8df7c90685396176ec
Jayaram <jayaram@heresy.in>
2014-10-07 19:24:51 +0530

    Authorization check was added before deleting a group.

--- reech_alpha/app/controllers/api/v2/groups_controller.rb
+++ reech_alpha/app/controllers/api/v2/groups_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V2
     class GroupsController < BaseController
       before_filter :require_current_user
+      before_filter :check_group_owner, only: [:destroy]
 
       def index
       	render "index.json.jbuilder"
@@ -16,6 +17,14 @@ module Api
           msg = {:status => 403, :message => "Asoociate not a friend",:group_ids=>params[:group_id] }
         end
         render json: msg
+      end
+
+      private
+
+      def check_group_owner
+        if Group.find(params[:id]).user != current_user
+          render json: {status: 401, message: "Un-Authorized action."}
+        end
       end
 
     end
