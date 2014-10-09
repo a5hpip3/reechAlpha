4a5441530f3c0c8fe062b6ab941d91625a09e435
srinivas <kpsrinivas@gmail.com>
2014-10-09 10:23:59 +0530

    Fix for chat.

--- reech_alpha/app/models/chat.rb
+++ reech_alpha/app/models/chat.rb
@@ -1,7 +1,10 @@
-class Chat < ActiveRecord::Base 
+class Chat < ActiveRecord::Base
   validates_presence_of :broadcasted_by
   #validates_presence_of :broadcasted_to
   validates_presence_of :message
+
+  attr_accessor :status
+  attr_accessible :status
 
   def broadcaster
   	User.find_by_reecher_id(self.broadcasted_by)
